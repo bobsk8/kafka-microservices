41bff18bc57740a1e49bbadc0cd712c31e2264d3
Rodrigo Prado <rodrigovianaprado@gmail.com>
2020-11-03 21:15:57 +0000

    Add common service

---
+++ common/src/services/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from '../entities/product.entity';
+
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductService {
+    constructor(
+        @InjectRepository(Product)
+        private productsRepository: Repository<Product>,
+    ) { }
+
+    findAll(): Promise<Product[]> {
+        return this.productsRepository.find();
+    }
+
+    findOne(id: string): Promise<Product> {
+        return this.productsRepository.findOne(id);
+    }
+
+    async remove(id: string): Promise<void> {
+        await this.productsRepository.delete(id);
+    }
+}
