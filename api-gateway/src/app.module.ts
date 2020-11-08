fe2c45d0615adf71fcd0df7b6d0212e13b5a6419
Rodrigo Prado <rodrigovianaprado@gmail.com>
2020-11-08 11:42:47 +0000

    Fix back

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     UserModule,
+    ProductModule,
     AuthModule,
   ],
   controllers: [AppController],
---
+++ api-gateway/src/product/product.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { ProductController } from './product.controller';
+
+@Module({
+    imports: [],
+    controllers: [ProductController],
+    providers: []
+})
+export class ProductModule { }
